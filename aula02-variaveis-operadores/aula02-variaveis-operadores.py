798513f9cd60852b4cf8314f3af070b913f481d5
leandrocavaccini <leandrocavaccini2613@gmail.com>
2026-03-20 22:31:58 -0300

    aula 2 variáveis e operadores

---
+++ aula02-variaveis-operadores/aula02-variaveis-operadores.py
@@ -0,0 +1,35 @@
+from rope.base.pycore import perform_soa_on_changed_scopes
+
+print('olá mundo')
+
+print(7 + 4)
+print ('7 + 4')
+print('7' + '4') # Concatenação de strings
+
+# comentário de linha de python
+'''
+Comentários
+de
+múltiplas
+linhas
+'''
+
+# Variáveis
+
+nome = 'Leandro'
+idade = 26
+peso = 75
+
+print(nome, idade, peso)
+print(f'oiiii, {nome}!!!!')
+
+# Inputs - simulação de forms no cmd
+
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+
+print('Oiiii', nome, '! Vc tem', idade, 'anos') #Ruim
+print(f'Oiiii {nome} vc tem {idade} anos')
+
+nova_idade = idade + 1
+print(nova_idade)
